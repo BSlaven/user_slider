4a98608e784877688d9c575b490afe00bcdf2d7b
Slaven B <slaven.bunijevac@gmail.com>
2022-03-07 21:33:17 +0100

    add loopin through people

--- src/App.jsx
+++ src/App.jsx
@@ -8,15 +8,26 @@ function App() {
   const [ myPeople, setPeople ] = useState(people);
   const [ index, setIndex ] = useState(0);
 
-  
-
   return (
-    <div className="App">
-      <h3 className='title'>Reviews</h3>
-      <section className="section">
-        
-      </section>
-    </div>
+    <section className="section">
+      <div className="title">
+        <h2><span>/</span>reviews</h2>
+      </div>
+      <div className="section-center">
+        {myPeople && myPeople.map((person, index) => {
+          const { id, image, name, title, quote } = person;
+          return (
+            <article key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              
+            </article>
+          )
+        })}
+      </div>
+    </section>
   )
 }
 
