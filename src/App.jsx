bea5e5655acedba05143df5f16ffce4d3dec8028
Slaven B <slaven.bunijevac@gmail.com>
2022-03-07 21:02:36 +0100

    add base styles to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
-import './App.css'
+import './App.css';
+import people from './data';
 
 function App() {
 
   return (
     <div className="App">
-
+      <h3 className='title'>Reviews</h3>
+      <section className="section">
+        <p>{people[0].name}</p>
+      </section>
     </div>
   )
 }
