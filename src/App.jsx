dc0b2e666375f2e143dd960b3d95535320927a40
Slaven B <slaven.bunijevac@gmail.com>
2022-03-08 11:33:53 +0100

    add classes conditionally

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
-// import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FiChevronRight, FiChevronLeft } from 'react-icons/fi';
+import { FaQuoteRight } from 'react-icons/fa';
 import './App.css';
 import people from './data';
 
@@ -14,18 +15,31 @@ function App() {
         <h2><span>/</span>reviews</h2>
       </div>
       <div className="section-center">
-        {myPeople && myPeople.map((person, index) => {
+        {myPeople && myPeople.map((person, personIndex) => {
           const { id, image, name, title, quote } = person;
+          let position = 'nextSlide';
+          if(personIndex === index) {
+            position = 'activeSlide'
+          }
+          if(personIndex === index - 1 || (index === 0 && personIndex === people.length - 1)) {
+            position = 'lastSlide';
+          }
           return (
-            <article key={id}>
+            <article className={position} key={id}>
               <img src={image} alt={name} className="person-img" />
               <h4>{name}</h4>
               <p className="title">{title}</p>
               <p className="text">{quote}</p>
-              
+              <FaQuoteRight className='icon' />              
             </article>
           )
         })}
+        <button className="prev">
+          <FiChevronLeft />
+        </button>
+        <button className="next">
+          <FiChevronRight />
+        </button>
       </div>
     </section>
   )
